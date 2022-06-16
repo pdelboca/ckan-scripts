489420c50ebb7c8eb169052972bc3c0b591c175f
pdelboca <patriciodelboca@gmail.com>
2022-06-16 16:21:16 +0200

    Add populate-activities script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ckan-scripts
+
 Useful bash scripts to interact with the CKAN Api
+
+To use them just export your API Token as an environment variable and execute the script.
+
+
+Example:
+
+```bash
+$ export API_TOKEN=<your_token>
+$ ./populate-activities.sh
+Finished dataset 1
+Finished dataset 2
+...
+```
---
+++ populate-activities.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# A simple script to add some datasets and update them.
+#
+# The main goal of this script is to create a database with a lot of activities to test some performance changes.
+
+
+for i in {1..10}; do
+  curl -X POST -H "Authorization: $API_TOKEN" -d "name=api-package-$i" "http://localhost:5000/api/action/package_create" > /dev/null 2>&1
+  for j in {1..10000}; do
+    curl -H "Authorization: $API_TOKEN" -d "notes=Update from API:$j&id=api-package-$i" "http://localhost:5000/api/action/package_update" > /dev/null 2>&1
+  done
+  echo "Finished dataset $i"
+done
